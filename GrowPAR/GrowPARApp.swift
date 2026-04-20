ac97c82790ed5667d88796a5a628de0b2a8e1e17
hfozturka <hfozturka@hotmail.com>
2026-04-20 11:37:40 +0200

    Initial Commit

---
+++ GrowPAR/GrowPARApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowPARApp.swift
+//  GrowPAR
+//
+//  Created by hfö on 20.04.26.
+//
+
+import SwiftUI
+
+@main
+struct GrowPARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrowPARTests/GrowPARTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrowPARTests.swift
+//  GrowPARTests
+//
+//  Created by hfö on 20.04.26.
+//
+
+import Testing
+@testable import GrowPAR
+
+struct GrowPARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
